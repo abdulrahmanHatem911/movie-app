a7882cbf9c2c7851a93d0e4ed405a1df74490d1d
abdulrahmanHatem911 <habdelrahman911@gmail.com>
2022-12-31 20:49:11 +0200

    update0.0.3

--- lib/core/utills/contant.dart
+++ lib/core/utills/contant.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-//https://api.themoviedb.org/3/movie/610150?api_key=47a5882632510c50e01d28853cd434c6
 class AppConstant {
   static const String BASE_URL = "https://api.themoviedb.org/3";
   static const String API_KEY = "ENTER_YOUR_API";
@@ -17,7 +16,6 @@ class AppConstant {
       "$BASE_URL/movie/popular?api_key=$API_KEY&page=$index";
   static String movieDetailsPath(int movieId) =>
       "$BASE_URL/movie/$movieId?api_key=$API_KEY";
-  //https://api.themoviedb.org/3/movie/610150/recommendations?api_key=47a5882632510c50e01d28853cd434c6
   static String recommendationsMoviePath(int movieId) =>
       "$BASE_URL/movie/$movieId/recommendations?api_key=$API_KEY";
 
@@ -31,10 +29,8 @@ class AppConstant {
       "$BASE_URL/person/$actorId/movie_credits?api_key=$API_KEY&&language=en-US";
   static String getSearchMoviePath(String query) =>
       "$BASE_URL/search/movie?api_key=$API_KEY&query=$query";
-  //https://api.themoviedb.org/3/movie/760161/videos?api_key=47a5882632510c50e01d28853cd434c6
   static String getMovieVideoPath(int movieId) =>
       "$BASE_URL/movie/$movieId/videos?api_key=$API_KEY";
-  //  https://api.themoviedb.org/3/search/movie?
   static const String baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
   static String imageUrl(String path) => '$baseImageUrl$path';
